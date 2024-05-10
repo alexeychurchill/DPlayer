cf24971fe33c8a1e4cda610e51a5873c6c45aa19
Oleksii Lytovchenko <oleksii.lytovchenko@gmail.com>
2024-05-10 04:32:04 +0300

    Setup edge-to-edge mode

--- app/src/main/java/io/alexeychurchill/clown/MainActivity.kt
+++ app/src/main/java/io/alexeychurchill/clown/MainActivity.kt
@@ -1,8 +1,12 @@
 package io.alexeychurchill.clown
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
 import io.alexeychurchill.clown.library.ui.LibraryScreen
 import io.alexeychurchill.clown.ui.theme.ClownTheme
@@ -11,10 +15,22 @@ import io.alexeychurchill.clown.ui.theme.ClownTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupEdgeToEdge()
         setContent {
             ClownTheme {
                 LibraryScreen()
             }
+        }
+    }
+
+    private fun setupEdgeToEdge() {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
+        // Workaround, which disables enforced NavBar contrast
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.isNavigationBarContrastEnforced = false
         }
     }
 }
--- app/src/main/java/io/alexeychurchill/clown/ui/theme/Theme.kt
+++ app/src/main/java/io/alexeychurchill/clown/ui/theme/Theme.kt
@@ -9,8 +9,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -43,18 +42,30 @@ fun ClownTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
+
+    SystemUiEffect(darkTheme = darkTheme)
 
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
         content = content
     )
+}
+
+@Composable
+private fun SystemUiEffect(darkTheme: Boolean) {
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        return
+    }
+
+    val window = (view.context as? Activity)?.window
+    LaunchedEffect(key1 = window, key2 = darkTheme) {
+        window?.let {
+            WindowCompat.getInsetsController(it, it.decorView).apply {
+                isAppearanceLightStatusBars = !darkTheme
+                isAppearanceLightNavigationBars = !darkTheme
+            }
+        }
+    }
 }
